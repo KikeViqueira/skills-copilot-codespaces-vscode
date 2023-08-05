12723c7b63d7c62663a9ecfa37f8d9915c3da6b5
KikeViqueira <127397239+KikeViqueira@users.noreply.github.com>
2023-08-05 18:38:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,86 @@
 // Create new server
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const Comment = db.Comment;
+const User = db.User;
+const Post = db.Post;
 
-// Import model
-const Comment = require("../models/Comment");
+// Get Comments
+router.get('/', (req, res) => {
+  Comment.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name']
+      },
+      {
+        model: Post,
+        attributes: ['title']
+      }
+    ]
+  }).then(comments => {
+    res.json(comments);
+  });
+});
 
-// Import middleware
-const auth = require("../middleware/auth");
+// Get Single Comment
+router.get('/:id', (req, res) => {
+  Comment.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ['name']
+      },
+      {
+        model: Post,
+        attributes: ['title']
+      }
+    ]
+  }).then(comment => {
+    res.json(comment);
+  });
+});
 
-// Import controllers
-const {
-  getComments,
-  getComment,
-  createComment,
-  updateComment,
-  deleteComment,
-} = require("../controllers/comments");
+// Create Comment
+router.post('/', (req, res) => {
+  Comment.create({
+    body: req.body.body,
+    userId: req.body.userId,
+    postId: req.body.postId
+  }).then(comment => {
+    res.json(comment);
+  });
+});
 
-// Routes
-router.route("/").get(getComments).post(auth, createComment);
+// Update Comment
+router.put('/:id', (req, res) => {
+  Comment.update(
+    {
+      body: req.body.body,
+      userId: req.body.userId,
+      postId: req.body.postId
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(comment => {
+    res.json(comment);
+  });
+});
 
-router
-  .route("/:id")
-  .get(getComment)
-  .put(auth, updateComment)
-  .delete(auth, deleteComment);
+// Delete Comment
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(comment => {
+    res.json(comment);
+  });
+});
 
 module.exports = router;
 
